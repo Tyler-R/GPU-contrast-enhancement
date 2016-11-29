010e5304d92e8de7613a4ec38e1b404488432d0c
Tyler-R <trubenui@sfu.ca>
2016-11-29 00:59:01 -0800

    Cleanup yuv contract enhancement method

--- contrast-enhancement.cu
+++ contrast-enhancement.cu
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include "hist-equ.cuh"
 
-PGM_IMG gpu_contrast_enhancement_gray_image(PGM_IMG img_in)
-{
+PGM_IMG gpu_contrast_enhancement_gray_image(PGM_IMG img_in) {
     PGM_IMG result;
     int histogram_buffer[256];
 
@@ -19,22 +18,22 @@ PGM_IMG gpu_contrast_enhancement_gray_image(PGM_IMG img_in)
     return result;
 }
 
-PPM_IMG contrast_enhancement_c_yuv(PPM_IMG img_in)
-{
+PPM_IMG gpu_contrast_enhancement_c_yuv(PPM_IMG img_in) {
     PPM_IMG result;
 
     int histogram_buffer[256];
 
-    YUV_IMG yuv_temp_image = rgb2yuv(img_in);
-    unsigned char *yuv_image_buffer = (unsigned char *)malloc(yuv_temp_image.h * yuv_temp_image.w * sizeof(unsigned char));
+    YUV_IMG yuv_temp_image = gpu_rgb2yuv(img_in);
+    int64_t image_size = yuv_temp_image.h * yuv_temp_image.w;
+    unsigned char *yuv_image_buffer = (unsigned char *)malloc(image_size * sizeof(unsigned char));
 
-    histogram(histogram_buffer, yuv_temp_image.img_y, yuv_temp_image.h * yuv_temp_image.w, 256);
-    histogram_equalization(yuv_image_buffer, yuv_temp_image.img_y, hist,yuv_temp_image.h * yuv_temp_image.w, 256);
+    histogram(histogram_buffer, yuv_temp_image.img_y, image_size, 256);
+    histogram_equalization(yuv_image_buffer, yuv_temp_image.img_y, histogram_buffer, image_size, 256);
 
     free(yuv_temp_image.img_y);
     yuv_temp_image.img_y = yuv_image_buffer;
 
-    result = yuv2rgb(yuv_temp_image);
+    result = gpu_yuv2rgb(yuv_temp_image);
     free(yuv_temp_image.img_y);
     free(yuv_temp_image.img_u);
     free(yuv_temp_image.img_v);
