c904c18113233b07433c95aac669b28c8f47f401
Tyler-R <trubenui@sfu.ca>
2016-11-29 20:36:18 -0800

    Add gpu based hsl methods to hist-equ.cuh

--- hist-equ.cuh
+++ hist-equ.cuh
@@ -4,6 +4,7 @@
 
 PGM_IMG gpu_contrast_enhancement_gray_image(PGM_IMG img_in);
 PPM_IMG gpu_contrast_enhancement_c_yuv(PPM_IMG img_in);
+PPM_IMG gpu_contrast_enhancement_c_hsl(PPM_IMG img_in);
 
 void gpu_make_histogram(int *histogram_output, unsigned char *img_in, int img_size, int nbr_bin);
 void gpu_histogram_equalization(unsigned char * img_out, unsigned char * img_in,
@@ -11,3 +12,6 @@ void gpu_histogram_equalization(unsigned char * img_out, unsigned char * img_in,
 
 YUV_IMG gpu_rgb2yuv(PPM_IMG color_image_input);
 PPM_IMG gpu_yuv2rgb(YUV_IMG yuv_image_input);
+
+HSL_IMG gpu_rgb2hsl(PPM_IMG color_image_input);
+PPM_IMG gpu_hsl2rgb(HSL_IMG img_in);
